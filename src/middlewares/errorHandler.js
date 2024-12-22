a0d5bad2fddf6a14581e23388b0ecfc4a499c912
unknown <badsnake1@gmail.com>
2024-12-22 19:18:00 +0200

    fix required on validation 2

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,7 +5,7 @@ export const errorHandler = (err, req, res, next) => {
     return res.status(err.status).json({
       status: err.status,
       message: err.message,
-      errors: err.errors || [],
+      // errors: err.errors || [],
     });
 
   res.status(500).json({
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,12 +1,18 @@
 import Joi from 'joi';
 
 export const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Username should be a string',
-    'string.min': 'Username should be at least {#limit} characters long',
-    'string.max': 'Username should be at most {#limit} characters long',
-    'any.required': 'Username is required',
-  }),
+  name: Joi.string()
+    .min(3)
+    .max(20)
+    .pattern(/^[a-zA-Z]+$/)
+    .required()
+    .messages({
+      'string.base': 'Username should be a string',
+      'string.min': 'Username should be at least {#limit} characters long',
+      'string.max': 'Username should be at most {#limit} characters long',
+      'string.pattern.base': 'Username should only contain letters',
+      'any.required': 'Username is required',
+    }),
   phoneNumber: Joi.string()
     .pattern(/^(\+380\d{9}|0\d{9})$/)
     .required(),
