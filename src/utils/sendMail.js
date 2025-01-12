c0fefca1e10c495d3ca0e4e102beaf3f00ed70f1
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-01-12 11:33:46 +0200

    fix error 500 for email

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,6 +3,8 @@ import nodemailer from 'nodemailer';
 import { SMTP } from '../constants/index.js';
 import { env } from './env.js';
 
+import createHttpError from 'http-errors';
+
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
@@ -11,11 +13,20 @@ const transporter = nodemailer.createTransport({
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
-    logger: true,
-    debug: true,
-
+  logger: true,
+  debug: true,
 });
 
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  try {
+    const email = await transporter.sendMail(options);
+    console.log('Email sent:', email.response);
+    return email;
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 };
